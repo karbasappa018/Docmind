295734a26be32093cdb3ad691ee7eddb70888f2e
karbasappa018 <karbasappatadakale@gmail.com>
2026-08-16 21:45:28 +0530

    Chat Response DTO

---
+++ src/main/java/com/docmind/dto/ChatResponseDto.java
@@ -0,0 +1,17 @@
+package com.docmind.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatResponseDto {
+    private String answer;
+    private String conversationId;
+    private List<CitationDto> citations;
+    private Long responseTimeMs;
+}
