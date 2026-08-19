dfabce63277c6b348595d16bc4085254963b131a
karbasappa018 <karbasappatadakale@gmail.com>
2026-08-19 21:49:42 +0530

    main class

---
+++ src/main/java/com/docmind/DocmindBackendApplication.java
@@ -0,0 +1,13 @@
+package com.docmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocmindBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocmindBackendApplication.class, args);
+	}
+
+}
